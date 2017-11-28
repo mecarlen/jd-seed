4f15712bb3dc2512cd481df330604a0ed9246632
mecarlen <mecarlen@gmail.com>
2017-11-28 14:07:32 +0800

    修正冒泡、简单选择、直接插入排序算法

--- jd-seed/jd-seed-exercise/src/main/java/com/jd/seed/exercise/algorithm/sort/insertion/JunitStraightInsertionSort.java
+++ jd-seed/jd-seed-exercise/src/main/java/com/jd/seed/exercise/algorithm/sort/insertion/JunitStraightInsertionSort.java
@@ -22,16 +22,28 @@ public class JunitStraightInsertionSort extends SortTemplate {
 	@Override
 	public void sort() {
 		for (int firstlevel = 1; firstlevel < numCount; firstlevel++) {
+			System.out.print("第" + firstlevel + "轮：");
 			if (nums[firstlevel] < nums[firstlevel - 1]) {
 				int secondlevel = firstlevel - 1;
 				int tmp = nums[firstlevel];
 				nums[firstlevel] = nums[firstlevel - 1];
-				while (secondlevel < nums[secondlevel]) {
+				while (secondlevel > 0 && nums[secondlevel] > tmp) {
 					nums[secondlevel + 1] = nums[secondlevel];
 					secondlevel--;
 				}
 				nums[secondlevel + 1] = tmp;
 			}
+			for (int i = 0; i <= firstlevel; i++) {
+				if (i == 0 && i == firstlevel)
+					System.out.print("[" + nums[i] + "]");
+				else if (i == 0)
+					System.out.print("[" + nums[i]);
+				else if (i == firstlevel)
+					System.out.print("," + nums[i] + "]");
+				else
+					System.out.print("," + nums[i]);
+			}
+			System.out.println();
 		}
 	}
 
