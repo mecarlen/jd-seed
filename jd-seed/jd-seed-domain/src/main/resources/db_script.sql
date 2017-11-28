8c27af40247fd001e95518fd6a12df03aaedd715
mecarlen <mecarlen@gmail.com>
2017-11-28 17:57:27 +0800

    上传SQL脚本

---
+++ jd-seed/jd-seed-domain/src/main/resources/db_script.sql
@@ -0,0 +1,16 @@
+﻿---------------------- 创建测试库demo ----------------------------------
+CREATE DATABASE demo DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+------城市表------
+CREATE TABLE dict_city(
+	id BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'pk',
+	zh_name VARCHAR(30) NOT NULL COMMENT '中文名称',
+	en_name VARCHAR(50) COMMENT '英文名称',
+	unity_code VARCHAR(30) NOT NULL COMMENT '统一编码',
+	zh_full_pin VARCHAR(50) COMMENT '中文名全拼',
+	zh_short_pin VARCHAR(10) COMMENT '中文名简拼',
+	priority TINYINT(2) NOT NULL DEFAULT 99 COMMENT '排序号',
+	state TINYINT(2) NOT NULL DEFAULT 1 COMMENT '状态,0->停用,1->在用,2->停用',
+	create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+	CONSTRAINT dict_city_pk PRIMARY KEY(id)
+) ENGINE=InnoDB CHARSET=utf8 COMMENT='城市表';
