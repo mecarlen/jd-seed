61cfd606903398477bd383e28fa126cbd2b22cd4
wangfenghua3 <wangfenghua@jd.com>
2019-03-08 10:17:45 +0800

    排序模块测试优化

---
+++ jd-seed-exercise/src/test/java/com/jd/seed/exercise/algorithm/sort/SortTest.java
@@ -0,0 +1,57 @@
+package com.jd.seed.exercise.algorithm.sort;
+
+import org.junit.Test;
+
+import com.jd.seed.exercise.algorithm.sort.bubble.BubbleSort;
+
+/**
+ * <pre>
+ * 排序
+ * 
+ * </pre>
+ * 
+ * @author mecarlen 2017年2月25日 下午4:19:50
+ */
+public class SortTest {
+
+	@Test
+	public void bubbleSort() {
+		BubbleSort bs = new BubbleSort();
+		bs.execute();
+	}
+
+	@Test
+	public void quickSort() {
+
+	}
+
+	@Test
+	public void insertionSort() {
+
+	}
+
+	@Test
+	public void selectionSort() {
+
+	}
+
+	@Test
+	public void mergeSort() {
+
+	}
+
+	@Test
+	public void heapSort() {
+
+	}
+
+	@Test
+	public void radixSort() {
+
+	}
+
+	@Test
+	public void shellSort() {
+
+	}
+}
